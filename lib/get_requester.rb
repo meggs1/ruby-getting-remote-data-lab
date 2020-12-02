2d50242b2ffed121c707a4deb4e9127492f431b3
meggs1 <megan.schertz@gmail.com>
2020-12-02 14:18:06 -0600

    commit tree a2470b246f9d136c083dbebc70d8031bb4ba0096

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,25 @@
-# Write your code here
+require "open-uri"
+require 'pry'
+require 'net/http'
+class GetRequester
+
+
+
+    def initialize(url)
+
+    end
+
+    def get_response_body
+        url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+        uri = URI.parse(url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+        JSON.parse(get_response_body)
+    end
+
+
+
+end
